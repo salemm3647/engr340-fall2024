0bb2a5ed71523cf2ac14270e5af40f8d05cbd9b3
salemm3647 <salemm@dukes.jmu.edu>
2024-10-29 10:50:55 -0400

    10/29/24

--- Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
@@ -8,11 +8,20 @@ path = '../../../data/ekg/mitdb_201.csv'
 # load data in matrix from CSV file; skip first two rows
 
 ### Your code here ###
+load = np.loadtxt(path, skiprows=2, delimiter=  ',')
 
 # save each vector as own variable
 
 ### Your code here ###
+seconds = load[:,0]
+MLII= load[:,1]
+V1= load[:,2]
 
 # use matplot lib to generate a single
 
 ### Your code here ###
+##plt.plot(seconds,MLII)
+plt.plot(seconds,V1)
+plt.xlabel('Time (s)')
+plt.ylabel('Signal')
+plt.show()
--- Module 7 - Signal Processing/3 - Practice/9.3.2 - Processing EKG Data/ekg-signal-processing-template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.2 - Processing EKG Data/ekg-signal-processing-template.py
@@ -18,8 +18,11 @@ signal_filepath = path_to_folder + database_name + ".csv"
 Step #1: load data in matrix from CSV file; skip first two rows. Call the data signal.
 """
 
-signal = 0
+signal = np.loadtxt(signal_filepath, skiprows=2, delimiter=  ',')
 ## YOUR CODE HERE ##
+seconds = signal[:,0]
+MLII= signal[:,1]
+V1= signal[:,2]
 
 """
 Step 2: (OPTIONAL) pass data through LOW PASS FILTER (fs=250Hz, fc=15, N=6). These may not be correctly in radians
@@ -32,19 +35,40 @@ Step 3: Pass data through weighted differentiator
 """
 
 ## YOUR CODE HERE ##
-
+diff = np.diff(V1)
 
 """
 Step 4: Square the results of the previous step
 """
  ## YOUR CODE HERE ##
-
+squares= np.square(diff)
 """
 Step 5: Pass a moving filter over your data
 """
-
+weight= [1,1,1,1,1,1,1,1,1,1]
+sums= np.convolve(squares, weight)
 ## YOUR CODE HERE
 # make a plot of the results. Can change the plot() parameter below to show different intermediate signals
 plt.title('Process Signal for ' + database_name)
-plt.plot(signal)
+plt.plot(V1)
+plt.xlabel('Time (s)')
+plt.ylabel('Signal')
+plt.show()
+
+plt.title('Diff Signal for ' + database_name)
+plt.plot(diff)
+plt.xlabel('Time (s)')
+plt.ylabel('Signal')
+plt.show()
+
+plt.title('Square Signal for ' + database_name)
+plt.plot(squares)
+plt.xlabel('Time (s)')
+plt.ylabel('Signal')
+plt.show()
+
+plt.title('Average Signal for ' + database_name)
+plt.plot(sums)
+plt.xlabel('Time (s)')
+plt.ylabel('Signal')
 plt.show()
