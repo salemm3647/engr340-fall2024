c90e53e20346d2e69fb919889e23e9f6a21c9467
salemm3647 <salemm@dukes.jmu.edu>
2024-11-07 11:51:03 -0500

    11/7/24

--- Module 7 - Signal Processing/4 - Assignments/9.4.1 - Pan-Tompkins Algorithm/pan-tompkins-template.py
+++ Module 7 - Signal Processing/4 - Assignments/9.4.1 - Pan-Tompkins Algorithm/pan-tompkins-template.py
@@ -1,4 +1,7 @@
 import numpy as np
+import scipy.signal
+from mpmath import diffs
+
 from ekg_testbench import EKGTestBench
 
 def detect_heartbeats(filepath):
@@ -16,29 +19,45 @@ def detect_heartbeats(filepath):
 
     # load data in matrix from CSV file; skip first two rows
     ## your code here
+    data = np.loadtxt(path, skiprows=2, delimiter=  ',')
 
     # save each vector as own variable
     ## your code here
+    seconds = data[:, 0]
+    MLII = data[:, 1]
+    V1 = data[:, 2]
 
     # identify one column to process. Call that column signal
 
-    signal = -1 ## your code here
+    signal = V1 ## your code here
 
     # pass data through LOW PASS FILTER (OPTIONAL)
     ## your code here
+
 
     # pass data through HIGH PASS FILTER (OPTIONAL) to create BAND PASS result
     ## your code here
 
     # pass data through differentiator
     ## your code here
+    for i in range(0,len(signal)):
+        diff = signal[i] - signal[i-1]
+        diffs.append(diff)
+
 
     # pass data through square function
     ## your code here
-
+    squares = []
+    for i in  range(0,len(diffs)):
+        square = (diffs[i])^2
+        squares.append(square)
     # pass through moving average window
     ## your code here
-
+    averages = []
+    for i in  range(0,len(squares)):
+        av = squares[i] + squares[i+1] + squares[i+2] + + squares[i+3] + + squares[i+4] + + squares[i+5] + squares[i+6] + squares[i+7] + squares[i+8] + squares[i+9] + squares[i+10]
+        averages.append(av)
+        
     # use find_peaks to identify peaks within averaged/filtered data
     # save the peaks result and return as part of testbench result
 
